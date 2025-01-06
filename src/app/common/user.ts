d501583d8fa41b3dab276736d9977937ceba4f7f
amarhbaneen <amaralzana@gmail.com>
2025-01-06 12:00:07 +0200

    init the angular project , and created 3 components 1. login 2. users list 3. user details

---
+++ src/app/common/user.ts
@@ -0,0 +1,10 @@
+export class user{
+  constructor(
+    public username:string,
+    public password:string,
+    public id:number,
+    public firstName:string,
+    public surname:string,
+  ) {
+  }
+}
